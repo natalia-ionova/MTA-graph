81876b5e66f8d705c97177543b8ef6255cfed7ec
natalia-ionova <nionova@unix2.csc.calpoly.edu>
2019-01-20 14:53:38 -0800

    paths test

--- graph_tests.py
+++ graph_tests.py
@@ -8,6 +8,11 @@ class TestList(unittest.TestCase):
         self.assertEqual(g.get_vertex('719').adjacent_to, ['719', 'F09', 'G22'])
         self.assertEqual(g.get_vertex('235').adjacent_to, ['235', 'D24', 'R31'])
 
+    def test_find_all_paths(self):
+        g = Graph('transfers.txt')
+        self.assertEqual(g.find_all_paths('639', 'Q01'), [['639', 'M20', 'Q01'], ['639', 'M20', 'R23', 'Q01'], \
+                                                          ['639', 'Q01'], ['639', 'R23', 'M20', 'Q01'], ['639', 'R23', 'Q01']])
+
 
 
 if __name__ == '__main__':
